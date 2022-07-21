4f5eb6b92ed16df2458cbbf4aa1767052011c348
danielromeo <danielromeo99@gmail.com>
2022-07-21 22:27:19 +0200

    fixing the hero section padding issue and cleaning up css

--- scripts/main.js
+++ scripts/main.js
@@ -633,8 +633,29 @@
         teamSliderOne();
         scrollToTop();
         portfolioFilter();
+        
     });
 
+
+    $(window).on('resize', function(){
+        if(window.innerWidth < 2034){ // when window is still very big but gets bellow 2034px is size... do this...
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container-fluid   ');
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container');
+        } else if (window.innerWidth < 1602) {
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container-fluid   ');
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container');
+        }else if(window.innerWidth >= 2034){ // when window gets bigger than 2034...
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').addClass('container-fluid');
+            $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container');
+        }
+    
+        // }else { 
+        // // when it becomes greater.. than ..px
+        //     $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').addClass('container-fluid');
+        //     $('.hero-area-v6 .p-l-50 .no-gutters .col-md-9').removeClass('container');
+        // }
+    });
+    
     /*---------------------
     === Window Scroll  ===
     ----------------------*/
